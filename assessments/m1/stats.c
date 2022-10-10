86709d93511d131a6ce0e3f847cc2970580aa5a4
jaeger17 <jaeger17@protonmail.com>
2022-10-10 19:15:51 -0400

    Add .clang-format file and convert source code to BARR-C Embedded Standard

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -2,24 +2,23 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
  * @file stats.c
  * @brief Module 1 Peer-graded assigment source code.
  *
- * Implements stats.h, utilizing all functionality for C programming proof-of-concept.
+ * Implements stats.h, utilizing all functionality for C programming
+ * proof-of-concept.
  *
  * @author Jaeger17
  * @date 10/10/2022
  *
  */
-
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,14 +28,15 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-int main(void) {
+int
+main(void)
+{
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-    
+    unsigned char test[SIZE]
+        = { 34, 201, 190, 154, 8,   194, 2,   6,   114, 88,  45,  76, 123, 87,
+            25, 23,  200, 122, 150, 90,  92,  87,  177, 244, 201, 6,  12,  60,
+            8,  2,   5,   67,  7,   87,  250, 230, 99,  3,   100, 90 };
+
     puts("Module 1 Submission - Stats\n");
 
     printf("%-10s", "array: ");
@@ -50,12 +50,13 @@ int main(void) {
 
     // print all
     print_statistics(test, SIZE);
-
 }
 
-void print_statistics(unsigned char *array, size_t sz)
+void
+print_statistics(unsigned char *array, size_t sz)
 {
-    if (NULL != array) {
+    if (NULL != array)
+    {
         // print the array min
         printf("%-10s%d\n", "min: ", find_minimum(array, sz));
 
@@ -70,108 +71,135 @@ void print_statistics(unsigned char *array, size_t sz)
     }
 }
 
-void print_array(unsigned char *array, size_t sz)
+void
+print_array(unsigned char *array, size_t sz)
 {
-    if (NULL != array) {
-        for (size_t i = 0; i < sz; i++) {
+    if (NULL != array)
+    {
+        for (size_t i = 0; i < sz; i++)
+        {
             printf("%d ", array[i]);
         }
 
         // print a newline after looping.
         puts("");
-    }  
+    }
 }
 
-double find_median(unsigned char *array, size_t sz)
+double
+find_median(unsigned char *array, size_t sz)
 {
-    if (NULL != array) {
+    if (NULL != array)
+    {
 
         double median = 0;
-        size_t middle = (sz / 2) - 1;  // subtract 1 because we have a 0 indexed array
-        
-        // if the number is even, the median is the average of the two middle points
+        size_t middle
+            = (sz / 2) - 1; // subtract 1 because we have a 0 indexed array
+
+        // if the number is even, the median is the average of the two middle
+        // points
         if (sz % 2 == 0)
         {
-            double medians_added = array[middle] + array[middle+1];
-            median = medians_added / 2;
+            double medians_added = array[middle] + array[middle + 1];
+            median               = medians_added / 2;
         }
         // if the count is odd, it is the middle data point in the array
-        else 
+        else
         {
             median = (double)array[middle];
-            
         }
 
         return median;
-
-    } else {
+    }
+    else
+    {
         return 0;
-    } 
+    }
 }
 
-double find_mean(unsigned char *array, size_t sz)
+double
+find_mean(unsigned char *array, size_t sz)
 {
-    if (NULL != array) {
+    if (NULL != array)
+    {
         // store the total in an unsigned int since the total can elapse
         // the storage alotment for an unsigned char.
         unsigned int total = 0;
 
-        for (size_t i = 0; i < sz; i++) {
+        for (size_t i = 0; i < sz; i++)
+        {
             total += array[i];
         }
 
-        return (double)total/sz;
-    } else {
+        return (double)total / sz;
+    }
+    else
+    {
         return 0;
-    } 
+    }
 }
 
-unsigned char find_maximum(unsigned char *array, size_t sz)
+unsigned char
+find_maximum(unsigned char *array, size_t sz)
 {
-    if (NULL != array) {
+    if (NULL != array)
+    {
         unsigned char max = array[0];
 
         // start at 1 because we already assign min to array[0]
-        for (size_t i = 1; i < sz; i++) {
-            if (array[i] > max) {
+        for (size_t i = 1; i < sz; i++)
+        {
+            if (array[i] > max)
+            {
                 max = array[i];
             }
         }
-        
+
         return max;
-    } else {
+    }
+    else
+    {
         return 0;
-    } 
+    }
 }
 
-unsigned char find_minimum(unsigned char *array, size_t sz)
+unsigned char
+find_minimum(unsigned char *array, size_t sz)
 {
-    if (NULL != array) {
+    if (NULL != array)
+    {
         unsigned char min = array[0];
 
         // start at 1 because we already assign min to array[0]
-        for (size_t i = 1; i < sz; i++) {
-            if (array[i] < min) {
+        for (size_t i = 1; i < sz; i++)
+        {
+            if (array[i] < min)
+            {
                 min = array[i];
             }
         }
 
         return min;
-    } else {
+    }
+    else
+    {
         return 0;
-    } 
+    }
 }
 
-void sort_array(unsigned char *array, size_t sz)
+void
+sort_array(unsigned char *array, size_t sz)
 {
     qsort(array, sz, sizeof(unsigned char), uchar_compare);
 }
 
-static int uchar_compare(const void *a, const void *b)
+static int
+uchar_compare(const void *a, const void *b)
 {
-    // convert void pointer to unsigned char pointer and dereference for comparison
-    unsigned char uchar1 = *(unsigned char*)a;
-    unsigned char uchar2 = *(unsigned char*)b;
+    // convert void pointer to unsigned char pointer and dereference for
+    // comparison
+    unsigned char uchar1 = *(unsigned char *)a;
+    unsigned char uchar2 = *(unsigned char *)b;
 
     // return the result of this logical comparison arithmetic
     if (uchar1 == uchar2)
@@ -187,4 +215,3 @@ static int uchar_compare(const void *a, const void *b)
         return -1;
     }
 }
-
--- assessments/m1/stats.h
+++ assessments/m1/stats.h
@@ -2,10 +2,10 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
@@ -21,52 +21,58 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
-
+/* Add Your Declarations and Function Comments here */
 
 /**
  * @brief print all statistics.
- * 
- * prints the statistics of an array including the minimum, maximum, mean, and median.
- * 
- * @param array (unsigned char *): a pointer to the start of an unsigned char array.
+ *
+ * prints the statistics of an array including the minimum, maximum, mean, and
+ * median.
+ *
+ * @param array (unsigned char *): a pointer to the start of an unsigned char
+ * array.
  * @param sz (size_t): size of the array.
- * 
+ *
  */
 void print_statistics(unsigned char *array, size_t sz);
 
 /**
  * @brief print the contents of an unsigned char array given the correct size.
- * 
- * @param array (unsigned char *): a pointer to the start of an unsigned char array.
+ *
+ * @param array (unsigned char *): a pointer to the start of an unsigned char
+ * array.
  * @param sz (size_t): size of the array.
  */
 void print_array(unsigned char *array, size_t sz);
 
 /**
- * @brief find the median of a sorted unsigned character array given the correct size.
- * 
+ * @brief find the median of a sorted unsigned character array given the correct
+ * size.
+ *
  * @note the array argument MUST be sorted for this to work properly.
- * 
- * @param array (unsigned char *): a pointer to the start of an unsigned char array.
+ *
+ * @param array (unsigned char *): a pointer to the start of an unsigned char
+ * array.
  * @param sz (size_t): size of the array.
- * 
+ *
  * @return double
  */
 double find_median(unsigned char *array, size_t sz);
 
 /**
  * @brief find the mean of an unsigned character array given the correct size.
- * 
- * @param array (unsigned char *): a pointer to the start of an unsigned char array.
+ *
+ * @param array (unsigned char *): a pointer to the start of an unsigned char
+ * array.
  * @param sz (size_t): size of the array.
  */
 double find_mean(unsigned char *array, size_t sz);
 
 /**
  * @brief find the median of an unsigned character array given the correct size.
- * 
- * @param array (unsigned char *): a pointer to the start of an unsigned char array.
+ *
+ * @param array (unsigned char *): a pointer to the start of an unsigned char
+ * array.
  * @param sz (size_t): size of the array.
  *
  * @return unsigned char
@@ -75,30 +81,32 @@ unsigned char find_maximum(unsigned char *array, size_t sz);
 
 /**
  * @brief find the median of an unsigned character array given the correct size.
- * 
- * @param array (unsigned char *): a pointer to the start of an unsigned char array.
+ *
+ * @param array (unsigned char *): a pointer to the start of an unsigned char
+ * array.
  * @param sz (size_t): size of the array.
-  * 
+ *
  * @return unsigned char
  */
 unsigned char find_minimum(unsigned char *array, size_t sz);
 
 /**
  * @brief Sort an unsgined character array given the correct size.
- * 
+ *
  * Utilizes qsort to sort the array in O(n*log(n)).
- * 
- * @param array (unsigned char *): a pointer to the start of an unsigned char array.
+ *
+ * @param array (unsigned char *): a pointer to the start of an unsigned char
+ * array.
  * @param sz (size_t): size of the array.
  */
 void sort_array(unsigned char *array, size_t sz);
 
 /**
  * @brief Compare function needed by qsort.
- * 
+ *
  * @param a (const void *) pointer to the first char
  * @param b (const void *) pointer to the second char
- * @return unsigned char 
+ * @return unsigned char
  */
 static int uchar_compare(const void *a, const void *b);
 
